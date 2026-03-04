1fafa529b4789c59e63d4fcc57b2ed315c6b87a0
Dhruwvaviya999 <dhruwvaviya123@gmail.com>
2026-03-04 20:22:57 +0530

    Completed App Router of Nextjs: Implement authentication with NextAuth and create login page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,19 +3,21 @@ import { z } from "zod/v4";
 import { revalidatePath } from "next/cache";
 import postgres from "postgres";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
-    invalid_type_error: 'Please select a customer.',
+    error: 'Please select a customer.',
   }),
   amount: z.coerce
     .number()
     .gt(0, { message: "Please enter an amount greater than $0." }),
   status: z.enum(["pending", "paid"], {
-    invalid_type_error: "Please select an invoice status.",
+    error: "Please select an invoice status.",
   }),
   date: z.string(),
 });
@@ -102,7 +104,6 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
@@ -115,3 +116,22 @@ export type State = {
   };
   message?: string | null;
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
